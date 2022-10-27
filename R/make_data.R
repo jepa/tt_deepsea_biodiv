d81dbad432db49ede5fee2ecd1ac6189e9a590c8
Joris Wiethase <j.wiethase@gmail.com>
2022-10-27 14:00:20 +0100

    Add data files, export data

--- R/make_data.R
+++ R/make_data.R
@@ -1,5 +1,32 @@
 library(picante)
+library(tidyverse)
 
-# Make and export community matrix
-data <- read.csv("data-raw/TEST_LIT+DD_ALL_SPP_2022-10-10.csv",header=T,sep=",",fileEncoding="latin1")[-2,]
-community_matrix <- picante::sample2matrix(data)
+# Community matrix
+data <- read.csv("data-raw/TEST_LIT+DD_ALL_SPP_2022-10-10.csv", header = T, sep = ",", fileEncoding = "latin1")[-2, ] %>% 
+      drop_na(Abundance)
+data <- data[nzchar(data$Site), ]
+
+community_matrix <- picante::sample2matrix(data) 
+
+# Remove sites without any species records
+empty_sites <- rownames(community_matrix[rowSums(community_matrix) == 0, ])
+community_matrix <- community_matrix[!rownames(community_matrix) %in% empty_sites, ]
+
+# Check for non-numeric columns
+length(names(sapply(community_matrix, is.numeric))) == length(colnames(community_matrix))
+
+# Species curve data
+specaccum_result <- specaccum(community_matrix, method = 'random', permutations = 100)  
+specaccum_df <-  data.frame(sites = specaccum_result$sites, richness = specaccum_result$richness, sd = specaccum_result$sd)
+
+# iNEXT data
+i.out <- iNEXT(community_matrix, q = 0, datatype = "abundance")
+
+# Export
+write.table(community_matrix, file = 'data-processed/CCZ_community_matrix.txt')
+write.csv(specaccum_df, file = 'data-processed/CCZ_specaccum.csv')
+save(i.out, file = 'data-processed/iNEXT_abundance.RData')
+
+
+test <- iNEXT::ChaoRichness(community_matrix, datatype = "abundance", conf = 0.95)
+test <- rowSums(community_matrix)
--- R/make_results_figures.R
+++ R/make_results_figures.R
@@ -1,27 +1,30 @@
-library(picante)
 library(vegan)
 library(tidyverse)
+library(iNEXT)
+theme_set(theme_linedraw(base_size = 12))
 
 # -----------------------------------------------------------------------------------------------------------------
-# Data import & preparation
+# Data import
 # -----------------------------------------------------------------------------------------------------------------
-data <- read.csv("data-raw/TEST_LIT+DD_ALL_SPP_2022-10-10.csv",header=T,sep=",",fileEncoding="latin1")[-2,]
-community_matrix <- picante::sample2matrix(data)
+species_descriptions <- read.csv("TEMP_papers_table_1980_on_2022-09-21.csv")  # Missing
+community_matrix <- read.table('data-processed/CCZ_community_matrix.txt')
+specaccum_df <- read.csv('data-processed/CCZ_specaccum.csv')
+load('data-processed/iNEXT_abundance.RData')
 
-
-test <- data %>% 
-      dplyr::group_by(Site, Species) %>% 
-      dplyr::summarise(cover = Abundance) %>%  
-      reshape::cast(.,  Site ~ Species, value = "cover")
-test[is.na(test)] <-  0
-rownames(test) <- test$Site
-test <- test[-1, ]
 # -----------------------------------------------------------------------------------------------------------------
-# Figure: Raw diversity
+# Figure: Raw data
 # -----------------------------------------------------------------------------------------------------------------
-
-
-
+ggplot(species_descriptions, aes(x = Year)) +
+      geom_line(aes(y = cumul_desc, colour = "cumul_desc"), size = 1) +
+      geom_line(aes(y = cumul_spp, colour = "cumul_spp"), size = 1) +
+      geom_line(aes(y = cumul_pubs, colour = "cumul_pubs"), size = 1) +
+      labs(x = "Year", y = "Cumulative totals") +
+      #  ggtitle("Cumulative totals of publications and descriptions (year 2000- 2021)") +
+      theme(legend.position = "none") +
+      scale_colour_manual("", 
+                          breaks = c("cumul_desc", "cumul_spp", "cumul_pubs"),
+                          values = c("black", "coral2", "steelblue"),
+                          labels = c("all descriptions", "new species", " publications"))
 
 
 
@@ -29,31 +32,18 @@ test <- test[-1, ]
 # -----------------------------------------------------------------------------------------------------------------
 # Figure: Family/species accumulation by sampling effort
 # -----------------------------------------------------------------------------------------------------------------
-curve <-  data.frame(sites = specaccum(test[, -1])$sites, richness = specaccum(test[, -1])$richness, sd = specaccum(test[, -1])$sd)
-ggplot(curve) +
-      geom_ribbon(aes(sites, ymin = richness-sd, ymax = richness + sd), alpha = .5) +
+sample_based_accum <- ggplot(specaccum_df) +
+      geom_ribbon(aes(sites, ymin = richness-sd, ymax = richness + sd), alpha = .3, fill = 'blue') +
       geom_line(aes(sites, richness)) +
-      xlab("Site") +
+      xlab("Number of samples") +
       ylab("Species richness") +
       theme(legend.title = element_blank(),
-            legend.position = c(.9, .5))
+            legend.position = c(.9, .5)); sample_based_accum
 
 
 # -----------------------------------------------------------------------------------------------------------------
 # Figure: Rarefaction curves
 # -----------------------------------------------------------------------------------------------------------------
+ggiNEXT(i.out, type = 3)
 
 
-rarecurve_var <- vegan::rarecurve(data.matrix) ## abundance based- whole CCZ
-rarefy_var <- vegan::rarefy(data.matrix, 90611) ## arg 'sample missing
-rrarefy_var <- vegan::rrarefy(data.matrix, 93456) ## arg 'sample missing
-test <- vegan::estimateR(data.matrix) ## abund of single sample - FOR CCZ ONLY DATASET
-
-## gives ACE estimator - what about chao2, ES2000
-
-## sample based
-test <- specpool(data.matrix)## sample based
-test <- poolaccum(data.matrix) ## try pool accum- this is sample based
-write.csv(as.data.frame(pool.aurora, "OUTPUT.csv"))
-
-rarefy(data.matrix, 93456, se= FALSE, MARGIN = 1)
